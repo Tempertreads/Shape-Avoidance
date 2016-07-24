67d6121bce2a82caaab6d4941ab9025d50f06a9a
Tempertreads <Tempertreads@users.noreply.github.com>
2016-07-24 11:36:47 -0400

    Add files via upload

---
+++ V1-1/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace Shape_Avoidance
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CirclePicture = new System.Windows.Forms.PictureBox();
+            this.CoreMechanicsTimer = new System.Windows.Forms.Timer(this.components);
+            this.VortexPicture = new System.Windows.Forms.PictureBox();
+            this.TrianglePicture = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Restart = new System.Windows.Forms.Button();
+            this.Level2 = new System.Windows.Forms.Button();
+            this.StageTimer = new System.Windows.Forms.Timer(this.components);
+            this.SquarePicture = new System.Windows.Forms.PictureBox();
+            this.Scorebox = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.ScoreTimer = new System.Windows.Forms.Timer(this.components);
+            this.StartButton = new System.Windows.Forms.Button();
+            this.TitleBox = new System.Windows.Forms.PictureBox();
+            this.Outofbounds = new System.Windows.Forms.PictureBox();
+            this.BackgroundPicture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CirclePicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VortexPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrianglePicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SquarePicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TitleBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Outofbounds)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CirclePicture
+            // 
+            this.CirclePicture.Image = ((System.Drawing.Image)(resources.GetObject("CirclePicture.Image")));
+            this.CirclePicture.Location = new System.Drawing.Point(120, 123);
+            this.CirclePicture.Name = "CirclePicture";
+            this.CirclePicture.Size = new System.Drawing.Size(30, 27);
+            this.CirclePicture.TabIndex = 0;
+            this.CirclePicture.TabStop = false;
+            // 
+            // CoreMechanicsTimer
+            // 
+            this.CoreMechanicsTimer.Interval = 1;
+            this.CoreMechanicsTimer.Tick += new System.EventHandler(this.CoreMechanicsTimer_Tick);
+            // 
+            // VortexPicture
+            // 
+            this.VortexPicture.Image = ((System.Drawing.Image)(resources.GetObject("VortexPicture.Image")));
+            this.VortexPicture.Location = new System.Drawing.Point(655, -2);
+            this.VortexPicture.Name = "VortexPicture";
+            this.VortexPicture.Size = new System.Drawing.Size(48, 266);
+            this.VortexPicture.TabIndex = 1;
+            this.VortexPicture.TabStop = false;
+            // 
+            // TrianglePicture
+            // 
+            this.TrianglePicture.Image = ((System.Drawing.Image)(resources.GetObject("TrianglePicture.Image")));
+            this.TrianglePicture.Location = new System.Drawing.Point(120, 99);
+            this.TrianglePicture.Name = "TrianglePicture";
+            this.TrianglePicture.Size = new System.Drawing.Size(31, 27);
+            this.TrianglePicture.TabIndex = 3;
+            this.TrianglePicture.TabStop = false;
+            this.TrianglePicture.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(48, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(154, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(274, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Visible = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(398, 13);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.Visible = false;
+            // 
+            // Restart
+            // 
+            this.Restart.Location = new System.Drawing.Point(299, 127);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(75, 23);
+            this.Restart.TabIndex = 8;
+            this.Restart.Text = "Restart";
+            this.Restart.UseVisualStyleBackColor = true;
+            this.Restart.Visible = false;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // Level2
+            // 
+            this.Level2.Location = new System.Drawing.Point(289, 127);
+            this.Level2.Name = "Level2";
+            this.Level2.Size = new System.Drawing.Size(95, 23);
+            this.Level2.TabIndex = 9;
+            this.Level2.Text = "New Shape?";
+            this.Level2.UseVisualStyleBackColor = true;
+            this.Level2.Visible = false;
+            this.Level2.Click += new System.EventHandler(this.Level2_Click);
+            // 
+            // StageTimer
+            // 
+            this.StageTimer.Enabled = true;
+            this.StageTimer.Interval = 10000;
+            this.StageTimer.Tick += new System.EventHandler(this.StageTimer_Tick);
+            // 
+            // SquarePicture
+            // 
+            this.SquarePicture.Image = ((System.Drawing.Image)(resources.GetObject("SquarePicture.Image")));
+            this.SquarePicture.Location = new System.Drawing.Point(121, 156);
+            this.SquarePicture.Name = "SquarePicture";
+            this.SquarePicture.Size = new System.Drawing.Size(30, 27);
+            this.SquarePicture.TabIndex = 10;
+            this.SquarePicture.TabStop = false;
+            this.SquarePicture.Visible = false;
+            // 
+            // Scorebox
+            // 
+            this.Scorebox.BackColor = System.Drawing.SystemColors.WindowText;
+            this.Scorebox.ForeColor = System.Drawing.Color.Red;
+            this.Scorebox.Location = new System.Drawing.Point(0, 18);
+            this.Scorebox.Name = "Scorebox";
+            this.Scorebox.ReadOnly = true;
+            this.Scorebox.Size = new System.Drawing.Size(55, 20);
+            this.Scorebox.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.WindowText;
+            this.textBox5.ForeColor = System.Drawing.Color.Red;
+            this.textBox5.Location = new System.Drawing.Point(0, 0);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(42, 20);
+            this.textBox5.TabIndex = 12;
+            this.textBox5.Text = "Score";
+            // 
+            // ScoreTimer
+            // 
+            this.ScoreTimer.Enabled = true;
+            this.ScoreTimer.Interval = 1000;
+            this.ScoreTimer.Tick += new System.EventHandler(this.ScoreTimer_Tick);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(499, 225);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 13;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // TitleBox
+            // 
+            this.TitleBox.Image = ((System.Drawing.Image)(resources.GetObject("TitleBox.Image")));
+            this.TitleBox.Location = new System.Drawing.Point(0, 0);
+            this.TitleBox.Name = "TitleBox";
+            this.TitleBox.Size = new System.Drawing.Size(703, 264);
+            this.TitleBox.TabIndex = 14;
+            this.TitleBox.TabStop = false;
+            // 
+            // Outofbounds
+            // 
+            this.Outofbounds.Image = ((System.Drawing.Image)(resources.GetObject("Outofbounds.Image")));
+            this.Outofbounds.Location = new System.Drawing.Point(0, -2);
+            this.Outofbounds.Name = "Outofbounds";
+            this.Outofbounds.Size = new System.Drawing.Size(703, 266);
+            this.Outofbounds.TabIndex = 15;
+            this.Outofbounds.TabStop = false;
+            this.Outofbounds.Visible = false;
+            // 
+            // BackgroundPicture
+            // 
+            this.BackgroundPicture.Image = ((System.Drawing.Image)(resources.GetObject("BackgroundPicture.Image")));
+            this.BackgroundPicture.Location = new System.Drawing.Point(0, -2);
+            this.BackgroundPicture.Name = "BackgroundPicture";
+            this.BackgroundPicture.Size = new System.Drawing.Size(658, 266);
+            this.BackgroundPicture.TabIndex = 2;
+            this.BackgroundPicture.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 260);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.TitleBox);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.Scorebox);
+            this.Controls.Add(this.Level2);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.VortexPicture);
+            this.Controls.Add(this.CirclePicture);
+            this.Controls.Add(this.TrianglePicture);
+            this.Controls.Add(this.SquarePicture);
+            this.Controls.Add(this.Outofbounds);
+            this.Controls.Add(this.BackgroundPicture);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CirclePicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VortexPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrianglePicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SquarePicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TitleBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Outofbounds)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundPicture)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox CirclePicture;
+        private System.Windows.Forms.Timer CoreMechanicsTimer;
+        private System.Windows.Forms.PictureBox VortexPicture;
+        private System.Windows.Forms.PictureBox TrianglePicture;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button Restart;
+        private System.Windows.Forms.Button Level2;
+        private System.Windows.Forms.Timer StageTimer;
+        private System.Windows.Forms.PictureBox SquarePicture;
+        private System.Windows.Forms.TextBox Scorebox;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Timer ScoreTimer;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.PictureBox TitleBox;
+        private System.Windows.Forms.PictureBox Outofbounds;
+        private System.Windows.Forms.PictureBox BackgroundPicture;
+    }
+}
+
---
+++ V1-1/Form1.cs
@@ -0,0 +1,376 @@
+﻿/***************************************************** 
+ * Code for game "Shape Avoidance"                   *
+ * Code Writen By Michael Osborne                    *
+ * VER "1.0"                                         *
+ *                                                   *
+*****************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shape_Avoidance
+{
+    public partial class Form1 : Form
+    { 
+        //Initializing Global Variables
+        Int32 RCXaxis = 700;
+        Int32 RCYaxis = 100;
+        Int32 GTXaxis = 700;
+        Int32 GTYaxis = 100;
+        Int32 GTD = 1;
+        Int32 GTACTIVE = 0;
+        Int32 BSXaxis = 700;
+        Int32 BSYaxis = 100;
+        Int32 BSACTIVE = 0;
+        Int32 Score = 0;        
+        Int32 Stagenumber = 0;
+
+        public Form1()
+        {
+            //Make form
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            //moves pictures to their starting positions after loading the form
+            CirclePicture.Location = new Point(RCXaxis, RCYaxis);
+            TrianglePicture.Location = new Point(GTXaxis, GTYaxis);
+            SquarePicture.Location = new Point(BSXaxis, BSYaxis);
+        }
+        //CorMechanicsTimer runs most of the game
+        private void CoreMechanicsTimer_Tick(object sender, EventArgs e)
+        {
+
+//Getting Mouse location
+            //gets mouse position in the form and puts it into X and Y co-ordinates
+            Point p = this.PointToClient(Cursor.Position);
+            int X = p.X;
+            int Y = p.Y;
+
+//Collision Detection            
+            //Comparing Mouse position to Circle Position
+             if (X >= RCXaxis - 15 && X <= RCXaxis + 15 && Y >= RCYaxis - 13 && Y <=RCYaxis + 13){
+                 CoreMechanicsTimer.Stop();
+                 StageTimer.Stop();
+                 ScoreTimer.Stop();
+                 Score = 0;
+                 Restart.Visible = true;
+            };
+            //Comparing Mouse Position to Triangle Position
+             if (X >= GTXaxis - 15 && X <= GTXaxis + 15 && Y >= GTYaxis - 13 && Y <= GTYaxis + 13)
+             {
+                 CoreMechanicsTimer.Stop();
+                 StageTimer.Stop();
+                 ScoreTimer.Stop();
+                 Score = 0;
+                 Restart.Visible = true;
+             };
+            //Comparing Mouse Position to Square Position
+             if (X >= BSXaxis - 15 && X <= BSXaxis + 15 && Y >= BSYaxis - 13 && Y <= BSYaxis + 13)
+             {
+                 CoreMechanicsTimer.Stop();
+                 StageTimer.Stop();
+                 ScoreTimer.Stop();
+                 Score = 0;
+                 Restart.Visible = true;
+             };
+            //Comparing Mouse Position to Vortex Position
+             if (X >= 655)
+             {
+                 CoreMechanicsTimer.Stop();
+                 StageTimer.Stop();
+                 ScoreTimer.Stop();
+                 Score = 0;
+                 Restart.Visible = true;
+             };
+
+//Restriction on game limits 
+            //Have you exited the form?
+             if (X > 703 | X < 0 | Y > 264 | Y < 0)
+             {
+                 Outofbounds.Visible = true;
+                 Restart.Visible = true;
+                 SquarePicture.Visible = false;
+                 CirclePicture.Visible = false;
+                 TrianglePicture.Visible = false;
+                 VortexPicture.Visible = false;
+                 CoreMechanicsTimer.Stop();
+                 StageTimer.Stop();
+                 ScoreTimer.Stop();
+                 Score = 0;
+                 
+             }
+//Red Circle Mechanics
+            //move circle left 
+            RCXaxis = RCXaxis - 5;
+            CirclePicture.Location = new Point(RCXaxis, RCYaxis);
+               
+            //reset Circle to right side at a random location
+            if (RCXaxis < 20){
+                Random randomCircle = new Random();
+                int RC = randomCircle.Next(Y - 25, Y + 25);
+                RCYaxis = RC;
+                RCXaxis = 700;
+            }
+        
+ //Green Triangle Mechanics
+            //does Green triangle exist in this level? (GACTIVE 1=yes/ 0 = no)
+            if (GTACTIVE == 1) { 
+
+                 GTXaxis = GTXaxis - 7;
+                 int GTV = 0;
+
+            Random randomTriY = new Random();
+            
+            //Green triangle is random but generally wants to go up
+                 if (GTD == 1)
+                  {            
+                     GTV = randomTriY.Next(0,6);
+                  }
+            //Green Triangle generally wants to go down
+                 else if (GTD == 2)
+                  {
+                     GTV = randomTriY.Next(2, 8);
+                  }
+            //Green Triangle Has no Preferance
+                 else if (GTD == 3)
+                  {
+                     GTV = randomTriY.Next(0, 8);
+                  }              
+
+            //Yaxis variation (GTV = 1-3 UP/ GTV= 4-6 Down)
+                 switch(GTV){
+                    //Up
+                 case 1: case 2: case 3:
+                    GTYaxis = GTYaxis - 5;
+                         if (GTYaxis <= 0)
+                         {
+                             GTD = 2;
+                         }
+                    break;
+                    //Down
+                 case 4: case 5: case 6:
+                    GTYaxis = GTYaxis + 5;
+                         if (GTYaxis >= 264)
+                         {
+                             GTD = 1;
+                         }
+                    break;
+                 }
+
+            //sets Triangle's next location
+                  TrianglePicture.Location = new Point(GTXaxis, GTYaxis);
+
+            //Resets Triangle to right side at random location when it reaches the left side
+                 if (GTXaxis < 20)
+                  {
+                   Random randomTrireset = new Random();
+                   int GTY = randomTrireset.Next(0, 200);
+                   GTYaxis = GTY;
+                   GTXaxis = 700;
+
+            //Determines direction of triangle
+                   Random rdmTriDirection = new Random();
+                   GTD = rdmTriDirection.Next(1, 4);
+                  }
+            }
+
+//Blue Square Mechanics
+            //The Blue Square is set to follow the mouse
+            //does the blue square exist? (1 = yes/ 0 = no)
+            if (BSACTIVE == 1)
+            {
+                //X and Y are the mouse coordinates
+                if (BSXaxis > X)
+                {
+                    BSXaxis = BSXaxis - 3;
+                }
+                if (BSXaxis < X)
+                {
+                    BSXaxis = BSXaxis + 3;
+                }
+                if (BSYaxis > Y)
+                {
+                    BSYaxis = BSYaxis - 1;
+                }
+                if (BSYaxis < Y)
+                {
+                    BSYaxis = BSYaxis + 1;
+                }
+                //Update Location
+                SquarePicture.Location = new Point(BSXaxis, BSYaxis);
+            }
+
+            //Code used to test game mechanics/functionality
+            Tests();
+        }
+       
+//A minor reset to the game 
+        private void ResetGame()
+        {
+            //Reset Shapes
+            RCXaxis = 700;
+            RCYaxis = 100;
+            GTXaxis = 700;
+            GTYaxis = 100;
+            BSXaxis = 700;
+            BSYaxis = 100;
+
+
+            CoreMechanicsTimer.Start();
+            StageTimer.Start();
+            ScoreTimer.Start();
+            Restart.Visible = false;
+
+        }
+//complete game reset (back to title)
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            GTACTIVE = 0;
+            BSACTIVE = 0;
+            Score = 0;
+            Stagenumber = 0;
+            RCXaxis = 700;
+            RCYaxis = 100;
+            GTXaxis = 700;
+            GTYaxis = 100;
+            BSXaxis = 700;
+            BSYaxis = 100;
+            CirclePicture.Location = new Point(RCXaxis, RCYaxis);
+            TrianglePicture.Location = new Point(GTXaxis, GTYaxis);
+            SquarePicture.Location = new Point(BSXaxis, BSYaxis);
+
+            SquarePicture.Visible = false;
+            CirclePicture.Visible = false;
+            TrianglePicture.Visible = false;
+            VortexPicture.Visible = false;
+            Restart.Visible = false;
+            TitleBox.Visible = true;
+            StartButton.Visible = true;
+        }
+
+//Move to next Level
+        private void Level2_Click(object sender, EventArgs e)
+        {
+             
+            Stagenumber++;
+               //Adds Shapes for each stage
+            switch(Stagenumber){
+                    //Triangle
+              case 1:
+                 GTACTIVE = 1;
+                 TrianglePicture.Visible = true;
+                 Level2.Visible = false;
+                 ResetGame();
+                 break;
+                    //Square
+               case 2:
+                 BSACTIVE = 1;         
+                 SquarePicture.Visible = true;
+                 Level2.Visible = false;
+                 ResetGame();
+                 break;
+                    //Default
+               default:            
+                 SquarePicture.Visible = true;
+                 Level2.Visible = false;
+                 ResetGame();
+                 break;
+            }
+        }
+
+//Timer for stage (10 sec)
+        private void StageTimer_Tick(object sender, EventArgs e)
+        {
+            //end of Stage
+            if (Stagenumber <= 1)
+            {
+                CoreMechanicsTimer.Stop();
+                Level2.Visible = true;
+                StageTimer.Stop();
+                ScoreTimer.Stop();
+            }
+        }
+
+//Timer for score (1 per second)
+        private void ScoreTimer_Tick(object sender, EventArgs e)
+        {
+            Score = Score + 1;
+            string ScoreString = Score.ToString();
+            Scorebox.Text = (ScoreString);
+        }
+
+//Starts game
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            TitleBox.Visible = false;
+            StartButton.Visible = false;
+            SquarePicture.Visible = true;
+            CirclePicture.Visible = true;
+            TrianglePicture.Visible = true;
+            VortexPicture.Visible = true;
+            BackgroundPicture.Visible = true;
+            Outofbounds.Visible = false;
+            CoreMechanicsTimer.Start();
+            ScoreTimer.Start();
+            StageTimer.Start();
+            
+        }
+
+//Test code for various mechanics
+        private void Tests()
+        {
+            //Testing Triangle
+            /*
+            textBox1.Visible = true;
+            string GTDS = GTD.ToString();
+            textBox1.Text = (GTDS);
+            */
+
+            //Testing Circle
+            /*textBox1.Visible = true;
+              textBox2.Visible = true;
+              textBox3.Visible = true;
+              textBox4.Visible = true;
+             
+            //Testing Collision Detection
+              if (X >= RCXaxis - 50)
+              {
+                  textBox1.Text = "True";
+              }
+              else textBox1.Text = "False";
+
+              if (X <= RCXaxis + 50)
+              {
+                  textBox2.Text = "True";
+              }
+              else textBox2.Text = "False";
+
+              if (Y >= RCYaxis - 40)
+              {
+                  textBox3.Text = "True";
+              }
+              else textBox3.Text = "False";
+
+              if (Y <= RCYaxis + 40)
+              {
+                  textBox4.Text = "True";
+              }
+              else textBox4.Text = "False";
+           
+                  */         
+
+        }
+        
+           
+   }
+}
